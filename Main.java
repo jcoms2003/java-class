796d38cdc7621472b21912eccc4a05525a9e2469
jcoms2003 <jcomsusmc2022@gmail.com>
2026-03-20 10:56:18 -0600

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Hello");
+		int x = 120;
+		int y = 10;
+		if (x > y) {
+			System.out.println("X is greater than Y");
+		}
+		else if (y > x) {
+			System.out.println("Y is greater than X");
+		}
+		else {
+			System.out.println("X is equal to Y");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+## General java work from my class
+- Brocken into labs and rec work
