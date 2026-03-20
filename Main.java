fc3b923072b232e14c21857311334048a0ce9b6c
jcoms2003 <jcomsusmc2022@gmail.com>
2026-03-20 11:19:03 -0600

    cond's and input

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main{
 	public static void main(String[] args){
+		
 		System.out.println("Hello");
 		int x = 120;
 		int y = 10;
@@ -12,5 +15,18 @@ public class Main{
 		else {
 			System.out.println("X is equal to Y");
 		}
+		int result = add(x, y);
+		System.out.println("The result of adding X and Y is: " + result);
 	}
+	public static int add(int a, int b) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the first number: ");
+		a = input.nextInt();
+		System.out.print("Enter the second number: ");
+		b = input.nextInt();
+		int sum = a + b;
+		System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+		return sum;
+	}
+
 }
